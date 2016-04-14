1b85f36d8ef292ead7839eff658972e0c17f9fee
Xiao Nan <road2stat@gmail.com>
2016-04-13 19:04:10 -0500

    fix stephens999/fiveMinuteStats#24

--- server.R
+++ server.R
@@ -8,26 +8,27 @@ shinyServer(function(input, output) {
     prior_sigma = input$'prior_sigma'
     data_mu     = input$'data_mu'
     data_sigma  = input$'data_sigma'
-    n = 1e+4
+    n = 1
+
+    y = seq(prior_mu - 5 * prior_sigma, prior_mu + 5 * prior_sigma, length.out = 100)
 
     set.seed(42)
-    x = seq(prior_mu - 5 * prior_sigma, prior_mu + 5 * prior_sigma, length.out = n)
     y_data = rnorm(n, data_mu, data_sigma)
     post_mu = ((prior_mu/prior_sigma^2) + ((sum(y_data))/data_sigma^2))/
       ((1/prior_sigma^2) + (n/data_sigma^2))
     post_sigma = sqrt(1/((1/prior_sigma^2) + (n/data_sigma^2)))
 
-    y_prior = dnorm(x, prior_mu, prior_sigma)
-    y_lik = dnorm(x, data_mu, data_sigma)
-    y_post = dnorm(x, post_mu, post_sigma)
+    y_prior = dnorm(y, prior_mu, prior_sigma)
+    y_lik = dnorm(y, data_mu, data_sigma)
+    y_post = dnorm(y, post_mu, post_sigma)
 
-    y_max = max(c(y_prior, y_lik))
+    y_max = max(c(y_prior, y_lik, y_post))
 
-    plot(x, y_prior, type = 'l', lty = 2, ylab = 'density', ylim=c(0, y_max))
-    lines(x, y_lik, type = 'l')
-    lines(x, y_post, type = 'l', col='red')
+    plot(y, y_prior, type = 'l', lty = 2, ylab = 'density', ylim=c(0, y_max))
+    lines(y, y_lik, type = 'l', col = 'blue')
+    lines(y, y_post, type = 'l', col='red')
 
-    legend('topright', col = c('black', 'black', 'red'),
+    legend('topright', col = c('black', 'blue', 'red'),
            lty = c(2, 1, 1), cex = 1, bty = 'n',
            legend = c('Prior', 'Likelihood', 'Posterior'))
 
