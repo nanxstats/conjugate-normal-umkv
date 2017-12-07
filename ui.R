7a89e3ae230f4a9f7ba4bb633e939d6022b2cc3e
Nan Xiao <road2stat@gmail.com>
2017-12-07 15:14:25 -0500

    adjust width

--- ui.R
+++ ui.R
@@ -6,16 +6,15 @@ shinyUI(fluidPage(
 
   sidebarLayout(
     sidebarPanel(
-
+      width = 2,
       numericInput("y_data", label = "Observed data:", value = 56, step = 1L),
       numericInput("data_sigma", label = "Known variance of data:", value = 20, min = 0, step = 1L),
       hr(),
       numericInput("prior_mu", label = "Prior μ:", value = 80, step = 1L),
       numericInput("prior_sigma", label = "Prior σ:", value = 10, min = 0, step = 1L)
-
     ),
-
     mainPanel(
+      width = 10,
       plotOutput("dist_plot")
     )
   )
